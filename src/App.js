9e972bbfa25f3ab1bfb1ab543e43ef78a9a51270
VisqueII <58799509+VisqueII@users.noreply.github.com>
2020-12-04 13:20:22 -0800

    First commit.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="app">
         <Route component={Home} path="/" exact />
         <Route component={NasaPhoto} path="/nasaphoto" />
       </div>
@@ -16,3 +16,13 @@ function App() {
 }
 
 export default App;
+
+
+/*
+  Steps to remember for package.json/pushing to github:
+    - npm install gh-pages
+    - git remote add origin repo_url
+    - add "homepage": url
+    - add "predeploy": "npm run build" => in scripts
+    - add "deploy": "gh-pages - build" => in scritps   
+  */
--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -27,9 +27,9 @@ export default function NasaPhoto() {
     return (
         <>
         <NavBar />
-        <div>
+        <div className="nasa-photo">
             {photoData.media_type === "image" ? (
-            <img src={photoData.url} alt={photoData.title}/>
+            <img src={photoData.url} alt={photoData.title} className="photo" />
             ) : (
                 <iframe
                     title="space-video"
@@ -43,8 +43,8 @@ export default function NasaPhoto() {
             )}
                 <div>
                     <h1>{photoData.title}</h1>
-                    <p>{photoData.date}</p>
-                    <p>{photoData.explanation}</p>
+                    <p className="date">{photoData.date}</p>
+                    <p className="explanation">{photoData.explanation}</p>
                 </div>
         </div>
         </>
