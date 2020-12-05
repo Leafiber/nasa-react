ccac072e36f517b90b058a2088f60e9b5c9b5315
VisqueII <58799509+VisqueII@users.noreply.github.com>
2020-12-05 12:01:51 -0800

    More updates.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { HashRouter as Router } from 'react-router-dom';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
